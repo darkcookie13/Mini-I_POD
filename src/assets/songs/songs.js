86f1ae68660f86d965a1a063a992775e2d3bf86a
Barnali Das <115207006+darkcookie13@users.noreply.github.com>
2023-09-10 09:00:34 +0530

    Initial commit

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./Kabhi-Tumhe.mp3";
+import music2 from "./attention.mp3";
+import music3 from "./dynamite.mp3";
+
+const songs = {
+  music1,
+  music2,
+  music3
+};
+
+export default songs;
